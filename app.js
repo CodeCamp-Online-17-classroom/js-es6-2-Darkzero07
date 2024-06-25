30b93ade94df0a82831d5d42d086ad2a1696d3ff
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-25 21:02:32 +0700

    js lab 146

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+// จงเขียนฟังก์ชัน filterOutOdds โดยพารามิเตอร์ของฟังก์ชันสามารถรับตัวเลขได้ไม่จำกัด และให้ return ค่าเป็น array ของเลขที่เป็นเลขคู่
+
+function filterOutOdds(...numbers) {
+  return numbers.filter((num) => num % 2 === 0);
+}
+
+console.log(filterOutOdds(1, 2, 3, 4, 5, 6)); // [2, 4, 6]
+console.log(filterOutOdds(7, 8, 9, 10, 11, 12)); // [8, 10, 12]
+console.log(filterOutOdds(15, 17, 19)); // []
+console.log(filterOutOdds()); // []
